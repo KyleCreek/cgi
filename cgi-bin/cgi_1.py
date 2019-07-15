21e2fd791a7a665c872a1f8ede5419f5336d5b9a
Kyle Creek <Kyle.A.Creek@gmail.com>
2019-07-15 14:20:49 -0700

    Adding all work completed while going through class activities.

--- cgi-bin/cgi_1.py
+++ cgi-bin/cgi_1.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 import cgi
+import cgitb
 
+# Enables capability of debugging. Generally a bad idea to include this
+# in a production setting.
+cgitb.enable()
+
+1/1
 
 cgi.test()
--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 import cgi
 import cgitb
+import os
 
 cgitb.enable()
 
 print("Content-type: text/plain")
 print()
 print("Your job is to make this work")
+
+# Create an instance of the field storage Class
+form = cgi.FieldStorage()
+
+# Obtain a list of the operands
+operands = form.getlist('operand')
+print(operands)
+
+# Calculate the Totals
+try:
+    #total = sum(map(int, operands))
+    # Define initial Total
+    total = 0 
+    for value in operands:
+        # Change String Value to Integer when adding
+        total = total + int(value)
+    body = "Your total is: {}".format(total)
+except (ValueError, TypeError):
+    body = "Unable to calculate a sum: please provide integer operands."
+
+# Display the content to the users.
+print("Content-type: text/plain")
+print()
+print(body)
